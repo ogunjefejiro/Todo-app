40d9b77033d4f23b34b8570d7787a5e9f86e43b5
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-05-07 14:10:32 +0100

    renamed components as jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import AddTodos from './components/AddTodos';
-import Header from './components/Header';
-import Todos from './components/Todos';
-import './index.css';
+import { useState } from "react";
+import AddTodos from "./components/AddTodos";
+import Header from "./components/Header";
+import Todos from "./components/Todos";
+import "./index.css";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const handleClick = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
-  }
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+  };
   const handleDelete = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id ? todo : ""))
-  }
+    setTodos(todos.filter((todo) => (todo.id !== id ? todo : "")));
+  };
   const addNewTodo = (text) => {
-    const id = Math.floor(Math.random() * 100) + 1 
-    const newTodo = {id, ...text}
-    setTodos([...todos, newTodo])
-  }
+    const id = Math.floor(Math.random() * 100) + 1;
+    const newTodo = { id, ...text };
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <div className="container">
-      <Header/>
-      <AddTodos onAdd={addNewTodo}/>
-     { todos.length > 0 ? <Todos todos={todos} onClick={handleClick} onDelete={handleDelete}/> : ""}
+      <Header />
+      <AddTodos onAdd={addNewTodo} />
+      {todos.length > 0 ? <Todos todos={todos} onClick={handleClick} onDelete={handleDelete} /> : ""}
     </div>
   );
 }
